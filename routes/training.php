be08869e149d5b34f08611d23cfb8ca9e1b6e473
Martins Damilola <damiladz004@gmail.com>
2026-03-09 05:27:10 +0100

    recents

--- routes/training.php
+++ routes/training.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\Instructor\TrainingController;
 use Illuminate\Support\Facades\Route;
 
 Route::domain('training.genchess.ng')
     ->middleware(['auth', 'verified', 'trainingportal'])
     ->group(function () {
         Route::redirect('/', '/dashboard');
-        Route::view('/dashboard', 'training.index');
+        Route::get('/dashboard', [TrainingController::class, 'index']);
     });
